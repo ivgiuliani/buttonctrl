8e0b9712f377203d048259edcedf394766e3d877
Ivan Giuliani <giuliani.v@gmail.com>
2020-05-08 19:02:02 +0100

    Project import

---
+++ README.md
@@ -0,0 +1,50 @@
+# buttonctrl
+
+Arduino helper library for translating push buttons state into click events
+
+## Example
+
+    #include <buttonctrl.h>
+
+    #define PIN_BUTTON D8
+    ButtonCtrl button = ButtonCtrl(PIN_BUTTON);
+
+    void setup() {
+      Serial.begin(9600);
+      Serial.setTimeout(2000);
+      while(!Serial) { }
+
+      button.begin();
+    }
+
+    void loop() {
+      const ButtonEvent btn_ev = button.handle();
+
+      if (btn_ev != None) {
+        Serial.println(ButtonCtrl::str_from_event(btn_ev));
+      }
+
+      switch (btn_ev) {
+        case Click:
+          Serial.println("Clicked!");
+          break;
+        case LongClick:
+          Serial.println("Long click!");
+          break;
+        default:
+          break;
+      }
+    }
+
+## Events
+
+- `Click`: the button has been clicked and then released. This is the
+  event you want to check if you want to trigger some action when the
+  button is clicked.
+- `LongClick`: the button has been pressed for longer than `long_click_ms`.
+  Note that a `Click` event will *not* be raised after a long click
+  (but `ClickDown` and `ClickUp` will be raised as normal). If you don't
+  need to handle long clicks, then you should handle `Click` and `LongClick`
+  events in the same way.
+- `ClickDown`: the button has just been pressed down
+- `ClickUp`: the button has just been released
---
+++ buttonctrl.h
@@ -0,0 +1,107 @@
+#ifndef __BUTTONCTRL_H__
+#define __BUTTONCTRL_H__
+
+#include <Arduino.h>
+
+enum ButtonEvent {
+  // Raised when nothing has happened.
+  None = 0,
+
+  // Raised when the button has just been clicked.
+  ClickDown = 1,
+
+  // Raised when the button has been clicked and then released.
+  Click = 2,
+
+  // Raised when the button is released.
+  ClickUp = 3,
+
+  // Raised when the button is pressed for a long time (configurable via
+  // `long_click_ms` in the initialiser).
+  LongClick = 4,
+};
+
+class ButtonCtrl {
+public:
+  ButtonCtrl(const uint8_t pin, const uint16_t long_click_ms = 1500) {
+    this->pin = pin;
+    this->long_click_ms = long_click_ms;
+  }
+
+  void begin() {
+    pinMode(pin, INPUT);
+    current_state = None;
+  }
+
+  ButtonEvent handle() {
+    const long now = millis();
+    const bool is_btn_released = digitalRead(pin) == LOW;
+
+    // We only want to take action right after the button has been
+    // released, we don't care about the button state at rest.
+    if (is_btn_released) {
+      switch(current_state) {
+        case ClickDown:
+          return emit(Click);
+        case Click:
+        case LongClick:
+          last_click = 0;
+          return emit(ClickUp);
+        default:
+          return emit(None);
+      }
+    }
+
+    // The button is pressed at this point, other than triggering
+    // the right ClickDown event we should check for how long this
+    // has been going on in case we need to trigger a LongClick.
+    switch(current_state) {
+      case None:
+        last_click = now;
+        return emit(ClickDown);
+      case ClickDown:
+      case LongClick:
+        if (now - last_click > long_click_ms) {
+          return emit(LongClick);
+        }
+        return emit(ClickDown);
+      default:
+        // It shouldn't be possible to get here.
+        return emit(None);
+    }
+  }
+
+  static const char *str_from_event(ButtonEvent event) {
+    switch (event) {
+      case ClickDown:
+        return "click_down";
+      case ClickUp:
+        return "click_up";
+      case Click:
+        return "click";
+      case LongClick:
+        return "long_click";
+      case None:
+      default:
+        return NULL;
+    }
+  }
+
+private:
+  uint16_t long_click_ms;
+  uint8_t pin;
+  uint32_t last_click = 0;
+
+  ButtonEvent current_state = None;
+
+  ButtonEvent emit(ButtonEvent event) {
+    if (current_state == event) {
+      // return None when state has not changed in between calls
+      return None;
+    }
+    current_state = event;
+    return event;
+  }
+};
+
+#endif
